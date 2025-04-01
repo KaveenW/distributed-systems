c8c4c028e16bdb4c8031932810fac8a5cb4f0d8a
KaveenW <117672888+KaveenW@users.noreply.github.com>
2025-04-01 13:33:06 +0530

    Updated Prject Structure for Azure Vm

--- submit-microservice/src/server.js
+++ submit-microservice/src/server.js
@@ -3,16 +3,29 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import path from 'path';
 import submitRoutes from './routes/submitRoutes.js';
+import categoryRoutes from './routes/categoryRoutes.js';
 
 // Load environment variables
 dotenv.config();
-
 // Initialize the express app
 const app = express();
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// MongoDB connection
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB Connected!");
+    } catch (err) {
+        console.error("MongoDB Connection Failed. Retrying in 5 seconds...");
+        setTimeout(connectDB, 5000); // Retry after 5 seconds
+    }
+};
+
+connectDB();
 
 // Get the current directory name using import.meta.url
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
@@ -22,6 +35,10 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 // Set up routes
 app.use('/submit', submitRoutes);
+app.use('/categories', categoryRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 4200;
 app.listen(PORT, () => console.log(`Submit service running on port ${PORT}`));
+
+// const swaggerDocument = yaml.load('./swagger.yaml');
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
